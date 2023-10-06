7ac96781256d50ad89526eb64402575d6f9b83b5
Vyacheslav <slava.shilin07@mail.ru>
2023-10-06 12:59:53 +0300

    Works on latest Valheim version

---
+++ Patch/DisplayMarketPlace.cs
@@ -0,0 +1,35 @@
+﻿using Extensions;
+using HarmonyLib;
+using UnityEngine;
+using static Marketplace.Modules.NPC.Market_NPC;
+using static CosmeticSlots.Plugin;
+
+namespace CosmeticSlots;
+
+[HarmonyPatch]
+internal class DisplayMarketPlace
+{
+    private static readonly int ChestTex = Shader.PropertyToID("_ChestTex");
+    private static readonly int ChestBumpMap = Shader.PropertyToID("_ChestBumpMap");
+    private static readonly int ChestMetal = Shader.PropertyToID("_ChestMetal");
+
+    [HarmonyPatch(typeof(NPCcomponent), nameof(NPCcomponent.EquipItemsOnModel), typeof(SkinnedMeshRenderer),
+        typeof(string), typeof(CapsuleCollider[]))] [HarmonyPostfix] [HarmonyWrapSafe]
+    public static void CosmeticSlots_DisplayCosmeticsPatch(NPCcomponent __instance, SkinnedMeshRenderer joint,
+        string prefab, CapsuleCollider[] capsule)
+    {
+        if (!prefab.IsGood()) return;
+        var itemDrop = ObjectDB.instance.GetItemPrefab(prefab)?.GetComponent<ItemDrop>();
+        if (itemDrop == null) return;
+        var armorMaterial = itemDrop.m_itemData.m_shared.m_armorMaterial;
+        var itemType = itemDrop.m_itemData.m_shared.m_itemType;
+
+        if (itemType == COSMETIC_CHEST && armorMaterial)
+        {
+            joint.material.SetTexture(ChestTex, armorMaterial.GetTexture(ChestTex));
+            joint.material.SetTexture(ChestBumpMap, armorMaterial.GetTexture(ChestBumpMap));
+            joint.material.SetTexture(ChestMetal, armorMaterial.GetTexture(ChestMetal));
+        }
+        __instance.AttachArmor(prefab.GetStableHashCode(), joint, capsule);
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,23 +1,35 @@
-﻿using System.Reflection;
-using BepInEx;
+﻿using BepInEx;
+using BepInEx.Bootstrap;
 using HarmonyLib;
 using UnityEngine;
 
 namespace CosmeticSlots;
 
 [BepInPlugin(ModGUID, ModName, ModVersion)]
+[BepInDependency("MarketplaceAndServerNPCs", BepInDependency.DependencyFlags.SoftDependency)]
 internal class Plugin : BaseUnityPlugin
 {
-    public const string ModName = "Frogger.CosmeticSlots", ModVersion = "1.1.0", ModGUID = "com." + ModName;
+    public const string ModName = "Frogger.CosmeticSlots", ModVersion = "1.3.0", ModGUID = "com." + ModName;
     public const ItemDrop.ItemData.ItemType COSMETIC_CHEST = (ItemDrop.ItemData.ItemType)30;
     public const ItemDrop.ItemData.ItemType COSMETIC_HELMET = (ItemDrop.ItemData.ItemType)31;
     internal static Harmony harmony;
     public static Plugin _self;
 
-    private void Awake()
+    private void Awake() 
     {
         _self = this;
-        harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), ModGUID);
+        harmony = new Harmony(ModGUID);
+        harmony.PatchAll(typeof(DisplayCosmeticsPatch));
+        harmony.PatchAll(typeof(EquipItemCosmeticPatch));
+        harmony.PatchAll(typeof(IsItemEquipedPatch));
+        harmony.PatchAll(typeof(ItemStandCosmeticsPatch));
+        harmony.PatchAll(typeof(SetCosmeticItemsTypePatch));
+
+        if (Chainloader.PluginInfos.ContainsKey("MarketplaceAndServerNPCs"))
+        {
+            Logger.LogMessage("Pathing Marketplace");
+            harmony.PatchAll(typeof(DisplayMarketPlace));
+        }
     }
 
     private void OnDestroy()
